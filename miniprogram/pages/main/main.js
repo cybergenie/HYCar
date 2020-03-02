ad5159b8e4091d8c95201b7e4aa4db8023c4db0f
cybergenie <jinwei.li@outlook.com>
2020-03-02 17:17:40 +0800

    更新

--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -5,36 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
+    swiperImgUrls:[
+      "//img.souche.com/20161117/jpg/284e46c7da1ba9af64ae2c9fda63aae1.jpg",
+      "//img.souche.com/20161117/jpg/c5bf2c29ae7178fd57dcd4fc7a09a89c.jpg",
+      "//img.souche.com/20161117/jpg/06a0f2253ed58da11aebcb5275b753c9.jpg",
+      "//img.souche.com/20161117/jpg/1b560324d2541dca60b04c1552108f6a.jpg",
+      "//img.souche.com/20161117/jpg/a1ff51260f3d930f173f56123b8ff219.jpg",
+      "//img.souche.com/20161117/jpg/c972bdfac1227a22a1d0f7d4a5783c7b.jpg"
+    ]
 
   },
 
   onClickSearchSubmit: function onClickSearchSubmit(e) {
-    this.getCarList(e.detail.content)
+    this.getCarListByModule(e.detail.content)
   },
 
-  getCarList: function (keyword) {
+  getCarListByModule: function (keyword) {
     var that = this;
     if (!keyword) {
       keyword = ''
     }
     wx.request({
-      url: 'https://wanxin.souche.com/api/search/car.json',
+      url: 'https://wanxin.souche.com/json/car/getCarListByModule.json',
       data: {
-        "keyword": keyword
+        "moduleId": 165456
       },
       success: function (res) {
-        var carList =  res.data.data.carList;
-        var pageNo = res.data.data.pageNo;
-        var pageNum = res.data.data.pageNum;
-        var pageSize = res.data.data.pageSize;
-        var totalNum = res.data.data.totalNum;
+        var carList =  res.data.data.items;
         console.log(res)
         that.setData({
-          carList:carList,
-          pageNo:pageNo,
-          pageNum:pageNum,
-          pageSize:pageSize,
-          totalNum:totalNum
+          carList:carList
         })
       }
     })
@@ -69,7 +69,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCarList()
+    this.getCarListByModule()
   },
 
   /**
