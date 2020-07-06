ce2e47ea5e1683ef1f12b3b8ee9dbdb85bffbcbc
Li Jinwei <jinwei.li@outlook.com>
2020-07-06 22:39:55 +0800

    更新filtertab

--- miniprogram/components/filtertabmodule/filtertabmodule.js
+++ miniprogram/components/filtertabmodule/filtertabmodule.js
@@ -11,13 +11,39 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    displaycar: "none",
+    currentTab:-1,
+    isShow: true,
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    tabNav: function (e) {
+  
+      if (this.data.currentTab === e.target.dataset.current) {
+        return false;
+      } else {
+        var showMode = e.target.dataset.current == 0;
+        this.setData({
+          currentTab: e.target.dataset.current,
+          isShow: showMode
+        })        
+      }
+      if(this.data.currentTab === "0"){
+        this.setData({
+          displaycar: "block",
+          displayprice:"none"
+        })
+      }
+      if(this.data.currentTab === "2"){
+        this.setData({
+          displaycar: "none",
+          displayprice:"block"
+        })
+      }
+      
+    }
   }
 })
